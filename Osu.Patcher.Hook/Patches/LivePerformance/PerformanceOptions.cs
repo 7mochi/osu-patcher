21b354d5ff582e089e6c200a3dc7244f525ca462
7mochi <flyingcatdm@gmail.com>
2026-08-17 21:06:08 -0500

    refactor: remove dead pp calculator dropdown

--- Osu.Patcher.Hook/Patches/LivePerformance/PerformanceOptions.cs
+++ Osu.Patcher.Hook/Patches/LivePerformance/PerformanceOptions.cs
@@ -1,37 +1,29 @@
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Osu.Stubs.GameModes.Options;
-using Osu.Stubs.Graphics;
 using Osu.Stubs.Wrappers;
-using Osu.Utils.Extensions;
-using static Osu.Patcher.Hook.Patches.CustomStrings.CustomStrings;
 
 namespace Osu.Patcher.Hook.Patches.LivePerformance;
 
 [UsedImplicitly]
 internal class PerformanceOptions : PatchOptions
 {
-    private static readonly OptionDropdown OptionDropdownStub = new(typeof(PerformanceCalculatorType));
-
     public override IEnumerable<object> CreateOptions() =>
     [
         CreateInGameDisplayOption(),
         CreateLeaderboardDisplayOption(),
-        CreatePerformanceTypeOption(),
     ];
 
     public override void Load(Settings config)
     {
         ShowPerformanceInGame.Value = config.ShowPerformanceInGame;
         ShowPerformanceOnLeaderboard.Value = config.ShowPerformanceOnLeaderboard;
-        PerformanceType.Value = config.PerformanceCalculator;
     }
 
     public override void Save(Settings config)
     {
         config.ShowPerformanceInGame = ShowPerformanceInGame.Value;
         config.ShowPerformanceOnLeaderboard = ShowPerformanceOnLeaderboard.Value;
-        config.PerformanceCalculator = PerformanceType.Value;
     }
 
     #region Options Creation
@@ -50,26 +42,6 @@ internal class PerformanceOptions : PatchOptions
         /* onChange: */ null,
     ]);
 
-    private static object CreatePerformanceTypeOption()
-    {
-        var dropdownOptions = new[]
-        {
-            pDropdownItem.Constructor.Invoke(["Bancho", PerformanceCalculatorType.Bancho]),
-            pDropdownItem.Constructor.Invoke(["Akatsuki", PerformanceCalculatorType.Akatsuki]),
-            pDropdownItem.Constructor.Invoke([
-                "Akatsuki if RX/AP else Bancho",
-                PerformanceCalculatorType.AkatsukiLimited,
-            ]),
-        }.ToType(pDropdownItem.Class.Reference);
-
-        return OptionDropdownStub.Constructor.Invoke([
-            /* title: */ AddOsuString("PatcherPerformance", "PP calculator"),
-            /* items: */ dropdownOptions,
-            /* bindable: */ PerformanceType.Bindable,
-            /* onChange: */ null,
-        ]);
-    }
-
     #endregion
 
     #region Bindables
@@ -79,12 +51,6 @@ internal class PerformanceOptions : PatchOptions
 
     public static readonly BindableWrapper<bool> ShowPerformanceOnLeaderboard =
         new(BindableType.Bool, false, Settings.Default.ShowPerformanceOnLeaderboard);
-
-    public static readonly BindableWrapper<PerformanceCalculatorType> PerformanceType = new(
-        BindableType.Object,
-        Settings.Default.PerformanceCalculator,
-        Settings.Default.PerformanceCalculator
-    );
 
     #endregion
 }
